2e9737dbeda9b650b1e32d0b12f3c1f944e7ee83
drx <schmidt@alpha-structure.com>
2024-11-03 14:51:37 +0100

    [ci-pipeline] Show Api Test Report

--- solawi-bid-backend/src/test/kotlin/org/solyton/solawi/bid/migrations/migration-tests.kt
+++ solawi-bid-backend/src/test/kotlin/org/solyton/solawi/bid/migrations/migration-tests.kt
@@ -7,6 +7,7 @@ import org.solyton.solawi.bid.module.db.migrations.Config
 import org.solyton.solawi.bid.module.db.migrations.migrate
 
 class MigrationTests {
-    @Schema@Test
+    //@Schema
+    @Test
     fun runMigrationsOnH2() = runBlocking { migrate(Config.H2) }
 }
