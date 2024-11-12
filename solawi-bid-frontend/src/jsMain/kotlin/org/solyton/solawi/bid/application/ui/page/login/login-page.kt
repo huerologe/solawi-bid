553087d08faf610109dda477d892ef0ee97ccca8
drx <schmidt@alpha-structure.com>
2024-11-12 18:15:31 +0100

    [setup ktor client] basic login form

--- solawi-bid-frontend/src/jsMain/kotlin/org/solyton/solawi/bid/application/ui/page/login/login-page.kt
+++ solawi-bid-frontend/src/jsMain/kotlin/org/solyton/solawi/bid/application/ui/page/login/login-page.kt
@@ -2,11 +2,72 @@ package org.solyton.solawi.bid.application.ui.page.login
 
 import androidx.compose.runtime.Composable
 import org.evoleq.compose.Markup
-import org.jetbrains.compose.web.dom.Text
+import org.evoleq.compose.label.Label
+import org.evoleq.language.Lang
+import org.evoleq.language.component
+import org.evoleq.language.get
+import org.evoleq.optics.storage.Storage
+import org.evoleq.optics.transform.times
+import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.dom.*
+import org.solyton.solawi.bid.application.data.Application
+import org.solyton.solawi.bid.application.data.i18N
+import org.solyton.solawi.bid.application.data.userData
+import org.solyton.solawi.bid.module.i18n.data.language
+import org.solyton.solawi.bid.module.user.password
+import org.solyton.solawi.bid.module.user.username
 
 @Markup
 @Composable
 @Suppress("FunctionName")
-fun LoginPage() {
-    Text("Login Page")
+fun LoginPage(storage: Storage<Application>) {
+    val userData = storage * userData
+    val texts = (storage * i18N * language).read() as Lang.Block
+    val loginFields = texts.component("solyton.authentication.login.fields")
+    Div(attrs = {
+        style{
+            marginTop(20.pc)
+            display(DisplayStyle("flex"))
+            justifyContent(JustifyContent.Center)
+        }
+    }) {
+        Div(attrs = {
+            style {
+                width(400.px)
+                height(300.px)
+
+            }
+        }) {
+            // Flex {
+            Div {
+                Label(loginFields["username"], id = "username")
+                Br()
+                TextInput((userData * username).read()) {
+                    id("username")
+                    onInput {
+                        console.log(it.value)
+                        (userData * username).write(it.value)
+                    }
+                }
+                Br()
+                Label(loginFields["password"], id = "password")
+                Br()
+                PasswordInput((userData * password).read()) {
+                    id("password")
+                    onInput { (userData * password).write(it.value) }
+                }
+            }
+            Div {
+                val buttonTexts = texts.component("solyton.authentication.login.buttons")
+
+                Button(attrs = {
+                    onClick {
+                        console.log(userData.read())
+                    }
+                }) {
+                    Text(buttonTexts["ok"])
+                }
+            }
+        }
+    }
 }
