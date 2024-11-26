300ff1b887e78a0a36b0de4b7c4e8ff81e164968
drx <schmidt@alpha-structure.com>
2024-11-26 14:45:59 +0100

    [Routing] css

--- solawi-bid-frontend/src/jsMain/kotlin/org/solyton/solawi/bid/module/authentication/component/login-form.kt
+++ solawi-bid-frontend/src/jsMain/kotlin/org/solyton/solawi/bid/module/authentication/component/login-form.kt
@@ -8,8 +8,8 @@ import org.evoleq.language.component
 import org.evoleq.language.get
 import org.evoleq.optics.storage.Storage
 import org.evoleq.optics.transform.times
-import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.*
+import org.solyton.solawi.bid.application.ui.style.form.*
 import org.solyton.solawi.bid.module.authentication.data.*
 
 @Markup
@@ -19,53 +19,44 @@ fun LoginForm(storage: Storage<LoginForm>, login: ()->Unit) {
     val userData = storage * user
     val texts = (storage * texts).read() as Lang.Block
     val loginFields = texts.component("solyton.authentication.login.fields")
+
     Div(attrs = {
-        style{
-            marginTop(20.pc)
-            display(DisplayStyle("flex"))
-            justifyContent(JustifyContent.Center)
-        }
+        style { formStyle() }
     }) {
-        Div(attrs = {
-            style {
-                width(400.px)
-                height(300.px)
+        Div(attrs = { style { fieldStyle() } }) {
+            Label(loginFields["username"], id = "username", labelStyle = formLabelStyle)
+
+            TextInput((userData * username).read()) {
+                style { textInputStyle() }
+                id("username")
+                onInput {
+                    (userData * username).write(it.value)
+                }
             }
-            onKeyDown {
-                if(it.key == "Enter") {
+        }
+        Div(attrs = { style { fieldStyle() } }) {
+            Label(loginFields["password"], id = "password", labelStyle = formLabelStyle)
+            PasswordInput((userData * password).read()) {
+                style { textInputStyle() }
+                id("password")
+                onInput { (userData * password).write(it.value) }
+            }
+        }
+
+        Div(attrs = {style { formControlBarStyle() }}) {
+            val buttonTexts = texts.component("solyton.authentication.login.buttons")
+
+            Button(attrs = {
+                onClick {
                     login()
                 }
+            }) {
+                Text(buttonTexts["ok"])
             }
-        }) {
-            // Flex {
-            Div {
-                Label(loginFields["username"], id = "username")
-                Br()
-                TextInput((userData * username).read()) {
-                    id("username")
-                    onInput {
-                        (userData * username).write(it.value)
-                    }
-                }
-                Br()
-                Label(loginFields["password"], id = "password")
-                Br()
-                PasswordInput((userData * password).read()) {
-                    id("password")
-                    onInput { (userData * password).write(it.value) }
-                }
-            }
-            Div {
-                val buttonTexts = texts.component("solyton.authentication.login.buttons")
-
-                Button(attrs = {
-                    onClick {
-                        login()
-                    }
-                }) {
-                    Text(buttonTexts["ok"])
-                }
+            Button{
+                Text("Registrieren")
             }
         }
     }
 }
+
