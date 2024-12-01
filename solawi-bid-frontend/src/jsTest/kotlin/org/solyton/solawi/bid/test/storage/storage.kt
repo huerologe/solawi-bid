6727dc1f583eb3d9ea72938f15839f73d9b4fdf6
drx <schmidt@alpha-structure.com>
2024-12-01 09:25:36 +0100

    test TestStorage

--- solawi-bid-frontend/src/jsTest/kotlin/org/solyton/solawi/bid/test/storage/storage.kt
+++ solawi-bid-frontend/src/jsTest/kotlin/org/solyton/solawi/bid/test/storage/storage.kt
@@ -3,9 +3,18 @@ package org.solyton.solawi.bid.test.storage
 import androidx.compose.runtime.*
 import org.evoleq.compose.Markup
 import org.evoleq.optics.storage.Storage
+import org.evoleq.optics.storage.onChange
+import org.evoleq.optics.storage.onWrite
+import org.evoleq.optics.transform.times
+import org.jetbrains.compose.web.testutils.ComposeWebExperimentalTestsApi
+import org.jetbrains.compose.web.testutils.runTest
 import org.solyton.solawi.bid.application.data.Application
 import org.solyton.solawi.bid.application.data.env.Environment
+import org.solyton.solawi.bid.application.data.userData
 import org.solyton.solawi.bid.module.user.User
+import org.solyton.solawi.bid.module.user.username
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 @Markup
 @Composable
@@ -27,4 +36,21 @@ fun TestStorage(): Storage<Application> {
                 newApplication -> application = newApplication
         }
     )
+}
+
+class TestStorageTest {
+    @OptIn(ComposeWebExperimentalTestsApi::class)
+    @Test fun testStorageTest() = runTest {
+        composition {
+
+            val storage = TestStorage()
+
+            val name = "Alfred"
+            (storage * userData * username).write(name)
+
+            val result = (storage * userData * username).read()
+
+            assertEquals(name, result)
+        }
+    }
 }
