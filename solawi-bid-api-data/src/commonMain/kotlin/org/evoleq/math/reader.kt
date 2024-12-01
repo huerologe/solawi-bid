3f244353b518c4f3813e87cb3102e559afeed30c
drx <schmidt@alpha-structure.com>
2024-12-01 09:26:45 +0100

    constructors for readers and writers

--- solawi-bid-api-data/src/commonMain/kotlin/org/evoleq/math/reader.kt
+++ solawi-bid-api-data/src/commonMain/kotlin/org/evoleq/math/reader.kt
@@ -4,6 +4,9 @@ typealias Reader<E, T> = (E)->T
 typealias Source<T> = Reader<Unit, T>
 
 @MathDsl
+fun <E, T> Reader(r: (E)->T): Reader<E, T> = r
+
+@MathDsl
 infix fun <E, S, T> Reader<E, S>.map(f: (S)->T): Reader<E, T> = {e -> f (this(e))}
 
 @MathDsl
