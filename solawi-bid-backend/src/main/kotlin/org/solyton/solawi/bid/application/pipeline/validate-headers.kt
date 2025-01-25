f5aef155e6b096550e2283ed9911f9364f0bbaf2
drx <schmidt@alpha-structure.com>
2025-01-25 12:42:35 +0100

    [SMA-33] validate that CONTEXT header is present

---
+++ solawi-bid-backend/src/main/kotlin/org/solyton/solawi/bid/application/pipeline/validate-headers.kt
@@ -0,0 +1,24 @@
+package org.solyton.solawi.bid.application.pipeline
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import kotlinx.serialization.json.Json
+import org.evoleq.ktorx.result.Result
+import org.evoleq.ktorx.result.ResultSerializer
+import org.solyton.solawi.bid.application.exception.ApplicationException
+
+fun Application.interceptAndValidateHeaders() {
+    intercept(ApplicationCallPipeline.Plugins) {
+        // validate that the CONTEXT Header is present
+        val contextHeader = call.request.headers["CONTEXT"]
+        if (contextHeader.isNullOrEmpty()) {
+            call.respondText(
+                Json.encodeToString(
+                    ResultSerializer(), Result.Failure.Message(ApplicationException.MissingContextHeader.message) as Result<String>),
+                status = HttpStatusCode.BadRequest
+            )
+            finish()
+        }
+    }
+}
