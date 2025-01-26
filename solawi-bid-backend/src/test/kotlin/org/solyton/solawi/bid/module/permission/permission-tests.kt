4dca1f20ff835e63fa44f44f250ecf292dc2c343
drx <schmidt@alpha-structure.com>
2025-01-26 17:52:25 +0100

    [SMA-34] Add failing test to prevent merge of pull requests

---
+++ solawi-bid-backend/src/test/kotlin/org/solyton/solawi/bid/module/permission/permission-tests.kt
@@ -0,0 +1,11 @@
+package org.solyton.solawi.bid.module.permission
+
+import org.solyton.solawi.bid.DbFunctional
+import kotlin.test.Test
+import kotlin.test.fail
+
+class PermissionTest {
+    @DbFunctional@Test fun testIsGranted() {
+        fail("Not implemented yet")
+    }
+}
