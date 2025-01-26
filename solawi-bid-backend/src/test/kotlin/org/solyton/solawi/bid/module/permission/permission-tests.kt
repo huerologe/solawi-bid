6f5230e331cf03c268caaa7d1f7ab0c427778528
drx <schmidt@alpha-structure.com>
2025-01-26 18:11:21 +0100

    [SMA-34] Add failing test to prevent merge of pull requests

--- solawi-bid-backend/src/test/kotlin/org/solyton/solawi/bid/module/permission/permission-tests.kt
+++ solawi-bid-backend/src/test/kotlin/org/solyton/solawi/bid/module/permission/permission-tests.kt
@@ -1,11 +1,61 @@
 package org.solyton.solawi.bid.module.permission
 
+import org.evoleq.exposedx.test.runSimpleH2Test
+import org.jetbrains.exposed.sql.insert
+import org.junit.jupiter.api.Test
 import org.solyton.solawi.bid.DbFunctional
-import kotlin.test.Test
-import kotlin.test.fail
+import org.solyton.solawi.bid.module.db.schema.*
+import org.solyton.solawi.bid.module.permission.action.db.isGranted
+import kotlin.test.assertTrue
 
-class PermissionTest {
-    @DbFunctional@Test fun testIsGranted() {
-        fail("Not implemented yet")
+
+class PermissionTests {
+    @DbFunctional@Test
+    fun testIsGranted() = runSimpleH2Test(
+        UsersTable,
+        ContextsTable,
+        RightsTable,
+        RolesTable,
+        RoleRightContexts,
+        UserRoleContext
+    ){
+        // Setup database entries
+        val user = UserEntity.new {
+            username = "x"
+            password = "y"
+        }
+        val readRight = RightEntity.new {
+            name = "READ"
+            description = ""
+        }
+        val updateRight = RightEntity.new {
+            name = "UPDATE"
+            description = ""
+        }
+        val role = RoleEntity.new {
+            name = "READER"
+            description = ""
+        }
+        val context = ContextEntity.new {
+            name = "APP"
+
+        }
+        RoleRightContexts.insert {
+            it[roleId] = role.id
+            it[contextId] = context.id
+            it[rightId] = readRight.id
+        }
+        UserRoleContext.insert {
+            it[contextId] = context.id
+            it[userId] = user.id
+            it[roleId] = role.id
+        }
+
+        // Test
+        val success = isGranted(user.id.value, context.name, readRight.name)
+        assertTrue { success }
+
+        val failure = !isGranted(user.id.value, context.name, updateRight.name)
+        assertTrue { failure }
     }
 }
