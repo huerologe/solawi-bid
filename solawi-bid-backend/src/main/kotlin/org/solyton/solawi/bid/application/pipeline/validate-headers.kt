97dc6214617fc67ccc98571e178842ed0fba9a86
drx <schmidt@alpha-structure.com>
2025-02-03 13:12:02 +0100

    [SMA-109, SMA-116] Fixed: CORS preflight request causes error during header-validation

--- solawi-bid-backend/src/main/kotlin/org/solyton/solawi/bid/application/pipeline/validate-headers.kt
+++ solawi-bid-backend/src/main/kotlin/org/solyton/solawi/bid/application/pipeline/validate-headers.kt
@@ -2,6 +2,7 @@ package org.solyton.solawi.bid.application.pipeline
 
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import kotlinx.serialization.json.Json
 import org.evoleq.ktorx.result.Result
@@ -9,22 +10,29 @@ import org.evoleq.ktorx.result.ResultSerializer
 import org.solyton.solawi.bid.application.exception.ApplicationException
 import org.solyton.solawi.bid.application.permission.Context
 import org.solyton.solawi.bid.application.permission.Header
+import org.solyton.solawi.bid.module.user.service.credentialsAreOK
 
 fun Application.interceptAndValidateHeaders() {
     intercept(ApplicationCallPipeline.Plugins) {
+        if(call.request.httpMethod == HttpMethod.Options) {
+            return@intercept
+        }
         // validate that the CONTEXT Header is present
         val contextHeader = call.request.headers[Header.CONTEXT]
         if (contextHeader.isNullOrEmpty()) {
-            // call.response.headers.append(Header.CONTEXT, Context.Empty.value)
+
+            call.response.headers.append(Header.CONTEXT, Context.Empty.value)
             call.respondText(
                 Json.encodeToString(
-                    ResultSerializer(), Result.Failure.Message(ApplicationException.MissingContextHeader.message) as Result<String>),
+                    ResultSerializer(),
+                    Result.Failure.Message(ApplicationException.MissingContextHeader.message) as Result<String>
+                ),
                 status = HttpStatusCode.BadRequest
             )
             finish()
-        }
-        else {
+        } else {
             call.response.headers.append(Header.CONTEXT, contextHeader)
         }
     }
+
 }
