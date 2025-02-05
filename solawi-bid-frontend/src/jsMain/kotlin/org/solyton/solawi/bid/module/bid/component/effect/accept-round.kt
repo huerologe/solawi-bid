b67ab6ca7086428f8e03049456bf9ffe1a4b9204
drx <schmidt@alpha-structure.com>
2025-02-05 19:50:46 +0100

    [SMA-11, SMA-127, SMA-136] Implement effect acceptRound

---
+++ solawi-bid-frontend/src/jsMain/kotlin/org/solyton/solawi/bid/module/bid/component/effect/accept-round.kt
@@ -0,0 +1,77 @@
+package org.solyton.solawi.bid.module.bid.component.effect
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import org.evoleq.compose.Markup
+import org.evoleq.optics.lens.Lens
+import org.evoleq.optics.storage.Storage
+import org.evoleq.optics.transform.times
+import org.solyton.solawi.bid.application.data.Application
+import org.solyton.solawi.bid.application.data.actions
+import org.solyton.solawi.bid.application.data.modals
+import org.solyton.solawi.bid.application.ui.page.auction.action.acceptRound
+import org.solyton.solawi.bid.module.bid.data.Auction
+import org.solyton.solawi.bid.module.bid.data.Round
+import org.solyton.solawi.bid.module.error.component.showErrorModal
+import org.solyton.solawi.bid.module.error.lang.errorModalTexts
+
+
+@Markup
+@Composable
+@Suppress("FunctionName")
+fun LaunchAcceptRound(
+    storage: Storage<Application>,
+    auction: Lens<Application, Auction>,
+    round: Round
+) {
+    LaunchedEffect(Unit) {
+        launch{
+            acceptRound(
+                storage = storage,
+                auction = auction,
+                round = round
+            )
+        }
+    }
+}
+
+@Markup
+@Suppress("FunctionName")
+fun TriggerAcceptRound(
+    storage: Storage<Application>,
+    auction: Lens<Application, Auction>,
+    round: Round
+) = CoroutineScope(Job()).launch{
+    acceptRound(
+        storage = storage,
+        auction = auction,
+        round = round
+    )
+}
+
+@Markup
+suspend fun acceptRound(
+    storage: Storage<Application>,
+    auction: Lens<Application, Auction>,
+    round: Round
+) = coroutineScope {
+    val actions = (storage * actions).read()
+    try {
+        actions.emit(
+            acceptRound(
+                auction,
+                round.roundId
+            )
+        )
+    } catch (exception: Exception) {
+        (storage * modals).showErrorModal(
+            errorModalTexts(
+                exception.message ?: exception.cause?.message ?: "Cannot Emit action 'AcceptRound'"
+            )
+        )
+    }
+}
