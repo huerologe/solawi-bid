413b08b1e5a52198639910edef0e30cdd650389d
drx <schmidt@alpha-structure.com>
2025-02-16 15:49:24 +0100

    [SMA-11, SMA-84] UI Stuff

--- solawi-bid-frontend/src/jsMain/kotlin/org/solyton/solawi/bid/application/data/device/media-type.kt
+++ solawi-bid-frontend/src/jsMain/kotlin/org/solyton/solawi/bid/application/data/device/media-type.kt
@@ -1,5 +1,7 @@
 package org.solyton.solawi.bid.application.data.device
 
+import kotlinx.browser.window
+
 sealed class DeviceType(open val minWidth: Int,open val maxWidth: Int, open val order: Int) {
     data object Empty: DeviceType(0,0, 0)
     data object Mobile : DeviceType(0,600, 1)
@@ -13,11 +15,25 @@ sealed class DeviceType(open val minWidth: Int,open val maxWidth: Int, open val 
     companion object {
         fun from(width: Double) = when{
             width < Mobile.maxWidth -> Mobile
+            width >= Mobile.maxWidth && getDeviceType() == "Mobile" -> Mobile
             width >= Tablet.minWidth && width < Tablet.maxWidth -> Tablet
+            width >= Tablet.maxWidth && getDeviceType() == "Tablet" -> Tablet
             width >= Laptop.minWidth && width < Laptop.maxWidth -> Laptop
             width >= Desktop.minWidth && width < Desktop.maxWidth -> Desktop
             else -> Huge
         }
+    }
+}
+
+fun getDeviceType(): String {
+    val width = window.innerWidth
+    val pixelRatio = window.devicePixelRatio
+    val isTouchDevice = js("('ontouchstart' in window)") as Boolean
+
+    return when {
+        isTouchDevice && pixelRatio > 1 && width <= 600 -> "Mobile"
+        isTouchDevice && pixelRatio > 1 && width <= 1024 -> "Tablet"
+        else -> "Desktop"
     }
 }
 
