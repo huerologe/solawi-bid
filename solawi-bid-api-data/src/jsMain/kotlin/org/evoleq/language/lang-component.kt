5e063643a5661dd2dc4cf505b3e7064aead3b80c
drx <schmidt@alpha-structure.com>
2025-02-21 00:13:46 +0100

    [SMA-11, SMA-84, SMA-152] Improve mobileStyles WIP

--- solawi-bid-api-data/src/jsMain/kotlin/org/evoleq/language/lang-component.kt
+++ solawi-bid-api-data/src/jsMain/kotlin/org/evoleq/language/lang-component.kt
@@ -5,7 +5,8 @@ import org.evoleq.math.Reader
 interface LangComponent {
     val path: String
 }
-
+@I18N
 val component: (LangComponent)->Reader<Lang, Lang.Block> = { c -> Reader { lang -> (lang as Lang.Block).component(c.path) } }
 
+@I18N
 val subComp: (String)->Reader<Lang, Lang.Block> = {c -> Reader { lang -> (lang as Lang.Block).component(c) }}
