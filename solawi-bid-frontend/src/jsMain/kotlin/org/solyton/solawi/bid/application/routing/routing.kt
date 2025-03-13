a382c0c51bd16651a13ba5996aa2cc61007fea74
drx <schmidt@alpha-structure.com>
2025-03-13 15:06:39 +0100

    [SMA-206] Manual: basic setup 1

--- solawi-bid-frontend/src/jsMain/kotlin/org/solyton/solawi/bid/application/routing/routing.kt
+++ solawi-bid-frontend/src/jsMain/kotlin/org/solyton/solawi/bid/application/routing/routing.kt
@@ -17,6 +17,7 @@ import org.solyton.solawi.bid.application.ui.page.auction.*
 import org.solyton.solawi.bid.application.ui.page.dashboard.DashboardPage
 import org.solyton.solawi.bid.application.ui.page.login.LoginPage
 import org.solyton.solawi.bid.application.ui.page.login.effect.LaunchLogoutEffect
+import org.solyton.solawi.bid.application.ui.page.manual.HowToBidPage
 import org.solyton.solawi.bid.application.ui.page.manual.ManualPage
 import org.solyton.solawi.bid.application.ui.page.sendbid.SendBidPage
 import org.solyton.solawi.bid.application.ui.page.sendbid.ShowQRCodePage
@@ -47,6 +48,10 @@ fun Routing(storage: Storage<Application>): Routes = Routing("/") {
     }
     route("manual") {
         component { ManualPage(storage) }
+
+        route("how-to-bid") {
+            component { HowToBidPage(storage) }
+        }
     }
     route("solyton") {
         wrap {
--- solawi-bid-frontend/src/jsMain/kotlin/org/solyton/solawi/bid/application/ui/page/manual/how-to-bid-page.kt
+++ solawi-bid-frontend/src/jsMain/kotlin/org/solyton/solawi/bid/application/ui/page/manual/how-to-bid-page.kt
@@ -2,29 +2,21 @@ package org.solyton.solawi.bid.application.ui.page.manual
 
 import androidx.compose.runtime.Composable
 import org.evoleq.compose.Markup
-import org.evoleq.compose.layout.Horizontal
 import org.evoleq.compose.layout.Vertical
-import org.evoleq.compose.routing.navigate
 import org.evoleq.optics.storage.Storage
 import org.jetbrains.compose.web.dom.H1
-import org.jetbrains.compose.web.dom.H2
-import org.jetbrains.compose.web.dom.P
 import org.jetbrains.compose.web.dom.Text
 import org.solyton.solawi.bid.application.data.Application
 import org.solyton.solawi.bid.application.ui.style.page.verticalPageStyle
 
-@Composable
 @Markup
+@Composable
 @Suppress("FunctionName")
-fun ManualPage(application: Storage<Application>) {
+fun HowToBidPage(application: Storage<Application>) {
+
     Vertical(verticalPageStyle) {
-        H1 { Text("Betriebsanleitung") }
+        H1 { Text("Wie man bietet") }
 
-        H2 { "Inhaltsverzeichnis" }
 
-        Horizontal {
-            P({onClick { navigate("/manual/how-to-bid") }}){Text("Wie man bietet")}
-        }
     }
 }
-
