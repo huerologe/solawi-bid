5063903f4c946d3301dfa3c62de2f47b6b2efd48
drx <schmidt@alpha-structure.com>
2025-03-13 15:14:35 +0100

    [SMA-206] Manual: outline text

--- solawi-bid-frontend/src/jsMain/kotlin/org/solyton/solawi/bid/application/ui/page/manual/how-to-bid-page.kt
+++ solawi-bid-frontend/src/jsMain/kotlin/org/solyton/solawi/bid/application/ui/page/manual/how-to-bid-page.kt
@@ -5,6 +5,7 @@ import org.evoleq.compose.Markup
 import org.evoleq.compose.layout.Vertical
 import org.evoleq.optics.storage.Storage
 import org.jetbrains.compose.web.dom.H1
+import org.jetbrains.compose.web.dom.H2
 import org.jetbrains.compose.web.dom.Text
 import org.solyton.solawi.bid.application.data.Application
 import org.solyton.solawi.bid.application.ui.style.page.verticalPageStyle
@@ -16,6 +17,16 @@ fun HowToBidPage(application: Storage<Application>) {
 
     Vertical(verticalPageStyle) {
         H1 { Text("Wie man bietet") }
+
+        H2{ Text("1. Scanne den QR-Code ")}
+
+
+        H2{ Text("2. Biete")}
+
+        H2{ Text("3. Überprüfe das Ergebnis ")}
+
+        H2{ Text("1. Teile den QR-Code")}
+
 
 
     }
