9ef64b010f9a2d91ae6d3853cd7fc4700cb3178e
drx <schmidt@alpha-structure.com>
2025-04-12 07:50:12 +0200

    [SMA-229] Repair / Update permission-tests

--- solawi-bid-backend/src/test/kotlin/org/solyton/solawi/bid/module/permission/permission-tests.kt
+++ solawi-bid-backend/src/test/kotlin/org/solyton/solawi/bid/module/permission/permission-tests.kt
@@ -14,32 +14,34 @@ import kotlin.test.fail
 class PermissionTests {
     @DbFunctional@Test
     fun testIsGranted() = runSimpleH2Test(
+        RoleRightContexts,
+        UserRoleContext,
         UsersTable,
         ContextsTable,
         RightsTable,
         RolesTable,
-        RoleRightContexts,
-        UserRoleContext
-    ) {
+
+        ) {
+        val ts = System.currentTimeMillis()
         // Setup database entries
         val user = UserEntity.new {
-            username = "x"
+            username = "x-$ts"
             password = "y"
         }
         val readRight = RightEntity.new {
-            name = "READ"
+            name = "READ-$ts"
             description = ""
         }
         val updateRight = RightEntity.new {
-            name = "UPDATE"
+            name = "UPDATE-$ts"
             description = ""
         }
         val role = RoleEntity.new {
-            name = "READER"
+            name = "READER-$ts"
             description = ""
         }
         val context = ContextEntity.new {
-            name = "APP"
+            name = "APP-$ts"
 
         }
         RoleRightContexts.insert {
@@ -70,6 +72,123 @@ class PermissionTests {
             isGranted(user.id.value, "C", readRight.name)
         } catch (exception : Exception) {
             assertEquals(PermissionException.NoSuchContext("C"), exception)
+        }
+    }
+
+
+    @DbFunctional@Test
+    fun testIsGranted1() = runSimpleH2Test(
+        RoleRightContexts,
+        UserRoleContext,
+        UsersTable,
+        ContextsTable,
+        RightsTable,
+        RolesTable,
+
+        ) {
+        val ts = System.currentTimeMillis()
+        // Setup database entries
+        val user = UserEntity.new {
+            username = "x-$ts"
+            password = "y"
+        }
+        val readRight = RightEntity.new {
+            name = "READ-$ts"
+            description = ""
+        }
+        val updateRight = RightEntity.new {
+            name = "UPDATE-$ts"
+            description = ""
+        }
+        val role = RoleEntity.new {
+            name = "READER-$ts"
+            description = ""
+        }
+        val context = ContextEntity.new {
+            name = "APP-$ts"
+
+        }
+        RoleRightContexts.insert {
+            it[roleId] = role.id
+            it[contextId] = context.id
+            it[rightId] = readRight.id
+        }
+        UserRoleContext.insert {
+            it[contextId] = context.id
+            it[userId] = user.id
+            it[roleId] = role.id
+        }
+
+        // Test
+        val success = isGranted(user.id.value, context.id.value, readRight.name)
+        assertTrue { success }
+
+        val failure = !isGranted(user.id.value, context.id.value, updateRight.name)
+        assertTrue { failure }
+
+        try {
+            isGranted(user.id.value, context.id.value, "A")
+        } catch (exception : Exception) {
+            assertEquals(PermissionException.NoSuchRight("A"), exception)
+        }
+
+
+    }
+
+    @DbFunctional@Test
+    fun testIsGranted2() = runSimpleH2Test(
+        RoleRightContexts,
+        UserRoleContext,
+        UsersTable,
+        ContextsTable,
+        RightsTable,
+        RolesTable,
+
+        ) {
+        val ts = System.currentTimeMillis()
+        // Setup database entries
+        val user = UserEntity.new {
+            username = "x-$ts"
+            password = "y"
+        }
+        val readRight = RightEntity.new {
+            name = "READ-$ts"
+            description = ""
+        }
+        val updateRight = RightEntity.new {
+            name = "UPDATE-$ts"
+            description = ""
+        }
+        val role = RoleEntity.new {
+            name = "READER-$ts"
+            description = ""
+        }
+        val context = ContextEntity.new {
+            name = "APP-$ts"
+
+        }
+        RoleRightContexts.insert {
+            it[roleId] = role.id
+            it[contextId] = context.id
+            it[rightId] = readRight.id
+        }
+        UserRoleContext.insert {
+            it[contextId] = context.id
+            it[userId] = user.id
+            it[roleId] = role.id
+        }
+
+        // Test
+        val success = isGranted(user.id.value, context.id.value, readRight.id.value)
+        assertTrue { success }
+
+        val failure = !isGranted(user.id.value, context.id.value, updateRight.id.value)
+        assertTrue { failure }
+
+        try {
+            isGranted(user.id.value, updateRight.id.value, readRight.id.value)
+        } catch (exception : Exception) {
+            assertEquals(PermissionException.NoSuchContext(updateRight.id.value.toString()), exception)
         }
     }
 }
